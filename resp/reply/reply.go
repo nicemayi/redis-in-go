ede0bc0b170d0853c26d0674e9f0da0611bcdd14
Zhe Wang <zwang96@fb.com>
2022-12-30 09:48:19 -0800

    add customized reply

--- resp/reply/reply.go
+++ resp/reply/reply.go
@@ -1,6 +1,98 @@
 package reply
 
+import (
+	"bytes"
+	"redis-in-go/interface/resp"
+	"strconv"
+)
+
 type ErrorReply interface {
 	Error() string
 	ToBytes() []byte
+}
+
+var (
+	nullBulkReplyBytes = []byte("$-1")
+	CRLF               = "\r\n"
+)
+
+type BulkReply struct {
+	Arg []byte
+}
+
+func (b *BulkReply) ToBytes() []byte {
+	if len(b.Arg) == 0 {
+		return nullBulkReplyBytes
+	}
+	return []byte("$" + strconv.Itoa(len(b.Arg)) + CRLF + string(b.Arg) + CRLF)
+}
+
+func MakeNulkReply(arg []byte) *BulkReply {
+	return &BulkReply{arg}
+}
+
+type MultiBulkReply struct {
+	Args [][]byte
+}
+
+func (r *MultiBulkReply) ToBytes() []byte {
+	argLen := len(r.Args)
+	var buf bytes.Buffer
+	buf.WriteString("*" + strconv.Itoa(argLen) + CRLF)
+
+	for _, arg := range r.Args {
+		if arg == nil {
+			buf.WriteString(string(nullBulkReplyBytes) + CRLF)
+		} else {
+			buf.WriteString("$" + strconv.Itoa(len(arg)) + CRLF + string(arg) + CRLF)
+		}
+	}
+
+	return buf.Bytes()
+}
+
+func MakeMultiBulkReply(args [][]byte) *MultiBulkReply {
+	return &MultiBulkReply{
+		Args: args,
+	}
+}
+
+type IntReply struct {
+	Code int64
+}
+
+func MakeIntReply(code int64) *IntReply {
+	return &IntReply{Code: code}
+}
+
+func (r *IntReply) ToBytes() []byte {
+	return []byte(":" + strconv.FormatInt(r.Code, 10) + CRLF)
+}
+
+type StatusReply struct {
+	Status string
+}
+
+func MakeStatusReply(status string) *StatusReply {
+	return &StatusReply{Status: status}
+}
+
+func (r *StatusReply) ToBytes() []byte {
+	return []byte("+" + r.Status + CRLF)
+}
+
+type StandardErrReply struct {
+	Status string
+}
+
+func (r *StandardErrReply) ToBytes() []byte {
+	return []byte("-" + r.Status + CRLF)
+}
+
+func (r *StandardErrReply) Error() string {
+	return r.Status
+}
+
+func IsErrReply(reply resp.Reply) bool {
+	return reply.ToBytes()[0] == '-'
 }

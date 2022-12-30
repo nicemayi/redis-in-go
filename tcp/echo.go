70c08e9a161083b46cf72726f93a51cbfe2ff048
Zhe Wang <zwang96@fb.com>
2022-12-30 00:39:55 -0800

    [2/2] improve handler

--- tcp/echo.go
+++ tcp/echo.go
@@ -1,8 +1,11 @@
 package tcp
 
 import (
+	"bufio"
 	"context"
+	"io"
 	"net"
+	"redis-in-go/lib/logger"
 	"redis-in-go/lib/sync/atomic"
 	"redis-in-go/lib/sync/wait"
 	"sync"
@@ -36,8 +39,35 @@ func (handler *EchoHandler) Handle(ctx context.Context, conn net.Conn) {
 	}
 
 	handler.activeConn.Store(client, struct{}{})
+	reader := bufio.NewReader(conn)
+
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				logger.Info("Connection close")
+				handler.activeConn.Delete(client)
+			} else {
+				logger.Warn(err)
+			}
+
+			return
+		}
+
+		client.Waiting.Add(1)
+		b := []byte(msg)
+		conn.Write(b)
+		client.Waiting.Done()
+	}
 }
 
 func (handler *EchoHandler) Close() error {
-	panic("dsad")
+	logger.Info("handler shutting down!")
+	handler.closing.Set(true)
+
+	handler.activeConn.Range(func(key, value interface{}) bool {
+		client := key.(*EchoClient)
+		client.Conn.Close()
+		return true
+	})
 }

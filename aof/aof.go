7fb898cd6b1b0cc561774204b31dc8402fbe23df
Zhe Wang <zwang96@fb.com>
2023-01-10 23:10:46 -0800

    implement aof functions

--- aof/aof.go
+++ aof/aof.go
@@ -2,10 +2,17 @@ package aof
 
 import (
 	"os"
+	"redis-in-go/config"
 	"redis-in-go/database"
+	"redis-in-go/lib/logger"
+	"redis-in-go/lib/utils"
+	"redis-in-go/resp/reply"
+	"strconv"
 )
 
 type CmdLine = [][]byte
+
+const aofBufferSize = 1 << 16
 
 type payload struct {
 	cmdLine CmdLine
@@ -21,9 +28,62 @@ type AofHandler struct {
 }
 
 // NewAofHandler
+func NewAofHandler(database database.Database) (*AofHandler, error) {
+	handler := &AofHandler{}
+	handler.aofFilename = config.Properties.AppendFilename
+	handler.database = database
+
+	// LoadAof
+	handler.LoadAof()
+
+	aofile, err := os.OpenFile(handler.aofFilename, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	// channel
+	handler.aofFile = aofile
+	handler.aofChan = make(chan *payload, aofBufferSize)
+	go func() {
+		handler.handleAof()
+	}()
+
+	return handler, nil
+}
 
 // Add payload(set k v) -> aofChan
+func (handler *AofHandler) AddAof(dbIndex int, cmd CmdLine) {
+	if config.Properties.AppendOnly && handler.aofChan != nil {
+		handler.aofChan <- &payload{
+			cmdLine: cmd,
+			dbIndex: dbIndex,
+		}
+	}
+}
 
 // handleAof payload(set k ) <- aofChan
+func (handler *AofHandler) handleAof() {
+	handler.currentDB = 0
+	for p := range handler.aofChan {
+		if p.dbIndex != handler.currentDB {
+			data := reply.MakeMultiBulkReply(utils.ToCmdLine("select", strconv.Itoa(p.dbIndex))).ToBytes()
+			_, err := handler.aofFile.Write(data)
+			if err != nil {
+				logger.Error(err)
+				continue
+			}
+			handler.currentDB = p.dbIndex
+		}
+
+		data := reply.MakeMultiBulkReply(p.cmdLine).ToBytes()
+		_, err := handler.aofFile.Write(data)
+		if err != nil {
+			logger.Error(err)
+		}
+	}
+}
 
 // LoadAof
+func (handler *AofHandler) LoadAof() {
+
+}

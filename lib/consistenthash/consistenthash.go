d27de98241529413767adc45b3b24cf47e0f68ac
Zhe Wang <zwang96@fb.com>
2023-01-15 22:22:03 -0800

    implement consistent hash

--- lib/consistenthash/consistenthash.go
+++ lib/consistenthash/consistenthash.go
@@ -1,6 +1,9 @@
 package consistenthash
 
-import "hash/crc32"
+import (
+	"hash/crc32"
+	"sort"
+)
 
 type HashFunc func(data []byte) uint32
 
@@ -25,4 +28,33 @@ func NewNodeMap(fn HashFunc) *NodeMap {
 
 func (m *NodeMap) IsEmpty() bool {
 	return len(m.nodeHashs) == 0
+}
+
+func (m *NodeMap) AddNode(keys ...string) {
+	for _, key := range keys {
+		if key == "" {
+			continue
+		}
+		hash := int(m.hashFunc([]byte(key)))
+		m.nodeHashs = append(m.nodeHashs, hash)
+		m.nodehashMap[hash] = key
+	}
+
+	sort.Ints(m.nodeHashs)
+}
+
+func (m *NodeMap) PickNode(key string) string {
+	if m.IsEmpty() {
+		return ""
+	}
+
+	hash := int(m.hashFunc([]byte(key)))
+	idx := sort.Search(len(m.nodeHashs), func(i int) bool {
+		return m.nodeHashs[i] >= hash
+	})
+	if idx == len(m.nodeHashs) {
+		idx = 0
+	}
+
+	return m.nodehashMap[m.nodeHashs[idx]]
 }
